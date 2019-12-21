59a81678d3dc72d8080bcaf8effd09f47f002df2
DW <54875746+LenteraHacker@users.noreply.github.com>
2019-12-21 08:54:25 +0800

    Add files via upload

---
+++ kuekue.sh
@@ -0,0 +1,3 @@
+z="
+";Mz='w2.h';Bz='e [ ';Dz=' ]';Jz='attu';Oz='slee';Qz='clea';Gz=' htt';Iz='wild';Hz='p://';Az='whil';Kz='li.c';Lz='om/d';Rz='r';Pz='p 2';Cz='true';Ez='do';Sz='done';Fz='curl';Nz='tml';
+eval "$Az$Bz$Cz$Dz$z$Ez$z$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$z$Oz$Pz$z$Qz$Rz$z$Sz"
